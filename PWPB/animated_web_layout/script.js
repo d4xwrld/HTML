6886b9778b51fa323a128240eb67a2a4097122cd
d4xwrld <reyzcraft87@gmail.com>
2023-09-11 08:51:52 +0700

    fix left panel responsive & add JS Calculate

--- PWPB/animated_web_layout/script.js
+++ PWPB/animated_web_layout/script.js
@@ -18,3 +18,26 @@ buyButton.addEventListener("click", () => {
     container.classList.add("right-panel-active");
   }
 });
+
+harga1 = 0;
+function updateHarga(menu) {
+  if (document.order.menu.value == 0) {
+    document.order.harga.value = null;
+  }
+  if (document.order.menu.value == 1) {
+    var harga1 = 15000;
+    document.order.harga.value = harga1;
+  } else if (document.order.menu.value == 2) {
+    var harga2 = 20000;
+    document.order.harga.value = harga2;
+  }
+}
+var porsiInput = document.getElementById("porsi");
+porsiInput.addEventListener("input", function () {
+  if (document.order.porsi.value > 0) {
+    var total = document.order.harga.value * document.order.porsi.value;
+    document.order.bayar.value = total;
+  } else {
+    document.order.bayar.value = "";
+  }
+});
