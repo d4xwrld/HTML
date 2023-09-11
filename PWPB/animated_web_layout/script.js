f61c91f534ed38457179ed03f015fd3d18529a7e
d4xwrld <reyzcraft87@gmail.com>
2023-09-11 07:10:14 +0700

    add new version [DIV]

--- PWPB/animated_web_layout/script.js
+++ PWPB/animated_web_layout/script.js
@@ -1,11 +1,20 @@
-const signUpButton = document.getElementById("signUp");
-const signInButton = document.getElementById("signIn");
+const BuyButton = document.getElementById("Buy");
+const BackButton = document.getElementById("Back");
+const buyButton = document.getElementById("buy");
 const container = document.getElementById("container");
 
-signUpButton.addEventListener("click", () => {
+BuyButton.addEventListener("click", () => {
   container.classList.add("right-panel-active");
 });
 
-signInButton.addEventListener("click", () => {
+BackButton.addEventListener("click", () => {
   container.classList.remove("right-panel-active");
+});
+
+buyButton.addEventListener("click", () => {
+  if (container.classList.contains("right-panel-active")) {
+    container.classList.remove("right-panel-active");
+  } else {
+    container.classList.add("right-panel-active");
+  }
 });
